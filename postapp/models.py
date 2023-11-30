212ae8ab9ba91a6593d2652accd87a7d7b6b9d2b
Aigerim <dairanbekaigerim@gmail.com>
2023-11-30 19:41:57 +0600

    Creation of Post and Tag models

---
+++ postapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from userapp.models import CustomUser
+from tagapp.models import Tag
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=2200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes_count = models.IntegerField(default=0)
+    comments_count = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return "Post by: " + self.author.username + "at " + self.created_at
+
+
+
+
+
